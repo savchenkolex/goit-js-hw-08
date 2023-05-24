caf09900a44a89be092309d0ccdae96ac89b5b44
lex <savchenko.lex@gmail.com>
2023-05-24 13:06:39 +0300

    01-gallery has been done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+
+galleryBox.insertAdjacentHTML("beforeend",createGalleryItems(galleryItems));
+
+function createGalleryItems(arr) {
+    return arr.map(({preview, original,description})=>{
+
+        return `<li class="gallery__item" >
+                    <a href="${original}" class="gallery__link">
+                        <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        data-original="${original}" 
+                        title="${description}" 
+                        width=300>
+                    </a>
+                </li>`;} ).join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a',{
+    caption:true, 
+    captionSelector: 'img',
+    captionData: 'Title',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+// console.log(galleryItems);
