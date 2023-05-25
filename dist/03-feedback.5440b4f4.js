53f09d2347f23034c715ecc4a45009505b20edbd
lex <savchenko.lex@gmail.com>
2023-05-25 16:00:16 +0300

    form handler

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -504,8 +504,38 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"eH52W":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-var _lodashThrottle = require("lodash.throttle"); //feedback-form-state
+var _lodashThrottle = require("lodash.throttle");
 var _lodashThrottleDefault = parcelHelpers.interopDefault(_lodashThrottle);
+const STORAGE_KEY = "feedback-form-state";
+const formEl = document.querySelector(".feedback-form");
+formEl.addEventListener("input", (0, _lodashThrottleDefault.default)(formElHandler, 500));
+formEl.addEventListener("submit", formSubmit);
+let formStateObj = {};
+const localFormState = localStorage.getItem(STORAGE_KEY);
+if (localFormState) {
+    formStateObj = JSON.parse(localFormState);
+    formEl.elements.email.value = formStateObj.email;
+    formEl.elements.message.value = formStateObj.message;
+}
+function formElHandler(event) {
+    const targetName = event.target.name;
+    const value = event.target.value;
+    if (targetName === "email") formStateObj.email = value;
+    else if (targetName === "message") formStateObj.message = value;
+    const savedString = {
+        ...formStateObj
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedString));
+}
+function formSubmit(event) {
+    event.preventDefault();
+    const elements = event.target.elements;
+    console.log(elements.email.value);
+    console.log(elements.message.value);
+    event.target.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.clear(STORAGE_KEY);
+}
 
 },{"lodash.throttle":"bGJVT","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"bGJVT":[function(require,module,exports) {
 var global = arguments[3];
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,42 @@
 import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector(".feedback-form");
+formEl.addEventListener("input",throttle(formElHandler,500));
+formEl.addEventListener("submit", formSubmit);
 
-//feedback-form-state
+let formStateObj = {};
+
+const localFormState = localStorage.getItem(STORAGE_KEY);
+
+if(localFormState) {
+    formStateObj = JSON.parse(localFormState);
+    formEl.elements.email.value = formStateObj.email;
+    formEl.elements.message.value = formStateObj.message;
+}
+
+function formElHandler(event){
+    const targetName = event.target.name;
+    const value = event.target.value;
+
+    if (targetName === "email") {
+        formStateObj.email = value;
+        
+    } else if (targetName === "message") {
+        formStateObj.message = value;
+    }
+    const savedString = {...formStateObj};
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(savedString));
+}
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.target.elements;
+    console.log(elements.email.value);
+    console.log(elements.message.value);
+    
+    event.target.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.clear(STORAGE_KEY);
+}
